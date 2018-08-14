9ed78b593d734e36b099e6a799f95749b7ecfd5c
Andrew Wyllie <wyllie@dilex.net>
2018-08-13 22:12:41 -0400

    rework dependency structure

--- tests/test_yacs.py
+++ tests/test_yacs.py
@@ -1,7 +1,8 @@
-from Yacs.yacs import Yacs
+
+import Yacs.yacs as y
 
 def test_init():
-    yac = Yacs()
+    yac = y.Yacs()
     assert yac.eye_type == 'oo'
     assert yac.wrap_len == 30
     assert yac.yac_saying == 'Moooo!'
