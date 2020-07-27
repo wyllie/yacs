1a7d2599dc9a9d6cb892f86418432e7c1cf5dc32
Andrew Wyllie <wyllie@dilex.net>
2020-07-27 15:07:10 -0400

    Bump version: 1.0.0 → 1.0.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     description='Yet Another CowSay',
     author='Andrew Wyllie',
     author_email='wyllie@dilex.net',
-    version='1.0.0',
+    version='1.0.1',
     packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
     license='MIT License',
     long_description=open('README.md').read()
